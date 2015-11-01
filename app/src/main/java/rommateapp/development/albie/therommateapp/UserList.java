8970da0fcad91ce382345deca1ce9b5ab1b84d1a
Alex Greco <grecoa66@students.rowan.edu>
2015-11-01 18:59:14 -0500

    added remove and add to userlist class

--- app/src/main/java/rommateapp/development/albie/therommateapp/UserList.java
+++ app/src/main/java/rommateapp/development/albie/therommateapp/UserList.java
@@ -1,14 +1,16 @@
 package rommateapp.development.albie.therommateapp;
 
+import java.util.ArrayList;
+
 /**
  * Created by alexgreco on 11/1/15.
  */
 public class UserList {
     public int userListId;
-    public User[] userList;
+    public ArrayList<User> userList;
     public boolean isEmpty;
 
-    public UserList(int userListId, User[] userList, boolean isEmpty) {
+    public UserList(int userListId, ArrayList<User> userList, boolean isEmpty) {
         this.userListId = userListId;
         this.userList = userList;
         this.isEmpty = isEmpty;
@@ -22,12 +24,20 @@ public class UserList {
         this.userListId = userListId;
     }
 
-    public User[] getUserList() {
+    public ArrayList<User> getUserList() {
         return userList;
     }
 
-    public void setUserList(User[] userList) {
+    public void setUserList(ArrayList<User> userList) {
         this.userList = userList;
+    }
+
+    public void addUser(User newUser){
+        userList.add(newUser);
+    }
+
+    public Boolean removeUser(User user){
+       return userList.remove(user);
     }
 
     public boolean isEmpty() {

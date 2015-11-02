931fcdf6ad64c0f33d09de099b651570c461b0fe
albiedoe <rynkie18@gmail.com>
2015-11-01 21:40:51 -0500

    add chore button functionality

--- app/src/main/java/rommateapp/development/albie/therommateapp/ChoreActivity.java
+++ app/src/main/java/rommateapp/development/albie/therommateapp/ChoreActivity.java
@@ -1,14 +1,23 @@
 package rommateapp.development.albie.therommateapp;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.astuetz.PagerSlidingTabStrip;
@@ -18,10 +27,12 @@ import com.astuetz.PagerSlidingTabStrip;
  */
 public class ChoreActivity extends AppCompatActivity {
 
+    private Context mContext;
     private FragmentManager fm;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chores_main);
+        mContext = this;
 
         //tab things
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
@@ -50,5 +61,50 @@ public class ChoreActivity extends AppCompatActivity {
         int id = item.getItemId();
         Utility.openNewActivity(id, this);
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void addChore(View view) {
+
+        LayoutInflater li = LayoutInflater.from(mContext);
+        View promptsView = li.inflate(R.layout.chore_add, null);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                mContext);
+
+        // set prompts.xml to alertdialog builder
+        alertDialogBuilder.setView(promptsView);
+
+        final EditText name = (EditText) promptsView
+                .findViewById(R.id.choreName);
+        final EditText desc = (EditText) promptsView
+                .findViewById(R.id.choreDesc);
+        final Spinner spinner = (Spinner) promptsView
+                .findViewById(R.id.PeopleSpinner);
+
+        // set dialog message
+        alertDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                // get user input and set it to result
+                                // edit text
+                                //result.setText(userInput.getText());
+                                Toast.makeText(mContext, name.getText().toString()+", "+desc.getText().toString()+", "+spinner.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
     }
 }
